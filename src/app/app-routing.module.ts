ba014c96dfc5a99f0932d707bf56edaacb09b612
Dhia Borji <dhia.borji2001@gmail.com>
2023-03-14 19:41:05 +0100

    house waste added with the root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,19 @@ import { AdminGuard } from './admin.guard';
 import { UsersListComponent } from './User/users-list/users-list.component';
 import { AdminDashboardComponent } from './Admin/admin-dashboard/admin-dashboard.component';
 import { DriversListComponent } from './Driver/drivers-list/drivers-list.component';
+import { HouseWasteComponent } from './Services/house-waste/house-waste.component';
 
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegistrationComponent},
-  { path: 'addDriver', component: AddComponent , canActivate: [AdminGuard] },
+  { path: 'addDriver', component: AddComponent , canActivate: [AdminGuard]},
   { path: 'login', component: LoginComponent},
   { path: 'usersList', component: UsersListComponent, canActivate: [AdminGuard]},
   { path: 'driversList', component: DriversListComponent, canActivate: [AdminGuard]},
+  { path: 'HouseWaste', component: HouseWasteComponent},
+
   { path: 'adminDashboard', component: AdminDashboardComponent, canActivate: [AdminGuard]}
 ];
 
